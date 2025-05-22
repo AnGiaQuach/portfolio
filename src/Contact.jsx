d46db5a20be51c2c3f0d2b232f1bc175f394e3e8
An Quach <163130718+AnGiaQuach@users.noreply.github.com>
2025-05-22 12:43:50 +0700

    add arrow icon to contactCard

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,19 +9,57 @@ export function Contact() {
 
       <div className={contactStyles.ContactContainer}>
         <div
+          onClick={() =>
+            (window.location.href = "https://github.com/AnGiaQuach")
+          }
           className={`${contactStyles.ContactCard} ${contactStyles.githubLink}`}
         >
           <a href="https://github.com/AnGiaQuach">
             https://github.com/AnGiaQuach
           </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={contactStyles.ArrowIcon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25"
+            />
+          </svg>
         </div>
         <div
           className={`${contactStyles.ContactCard} ${contactStyles.emailLink}`}
+          onClick={() =>
+            (window.location.href = "mailto:fartinghous@gmail.com")
+          }
         >
           <a href="mailto:fartinghous@gmail.com">fartinghous@gmail.com</a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={contactStyles.ArrowIcon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25"
+            />
+          </svg>
         </div>
         <div
           className={`${contactStyles.ContactCard} ${contactStyles.instaLink}`}
+          onClick={() =>
+            (window.location.href =
+              "https://www.instagram.com/an_thich_uong_latte/")
+          }
         >
           <a
             style={{ color: "deeppink" }}
@@ -29,6 +67,20 @@ export function Contact() {
           >
             https://www.instagram.com/an_thich_uong_latte/
           </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={contactStyles.ArrowIcon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25"
+            />
+          </svg>
         </div>
       </div>
     </div>
